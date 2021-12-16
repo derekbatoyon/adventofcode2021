4566275dbcbd9f0a0583cbd6cf1502b31c186bf4
Derek Batoyon <derek@bytemeister.com>
2021-12-15 20:34:25 -0800

    Use curses library to visualize path search.

--- day15/part2.py
+++ day15/part2.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3
 
 from functools import cache
-from colored import attr
 
 import argparse
+import curses
 import fileinput
 import operator
 import sys
+import time
 
 size_factor = 5
 
@@ -22,6 +23,24 @@ class Cave:
         for row in self.risk_level[1:]:
             assert self.cols == len(row)
         self.goal = (size_factor*self.rows-1, size_factor*self.cols-1)
+        self.window = None
+
+    def set_window(self, window):
+        self.window = window
+        if self.window:
+            self.attr = { False: curses.color_pair(0), True:  curses.color_pair(1) | curses.A_BOLD }
+
+    def draw(self, path=[]):
+        if self.window:
+            self.window.clear()
+
+            for x in range(size_factor*self.rows):
+                for y in range(size_factor*self.cols):
+                    position = (x, y)
+                    self.window.addch(x, y, self.calculate_risk_ch(position), self.attr[position in path])
+
+            self.window.refresh()
+            time.sleep(0.05)
 
     def adjacents(self, position):
         x, y = position
@@ -39,6 +58,9 @@ class Cave:
         risk += int(x / self.rows) + int(y / self.cols)
         return Cave.risk_adjustment[risk]
 
+    def calculate_risk_ch(self, position):
+        return self.calculate_risk(position) + ord('0')
+
     @cache
     def search_paths(self, position):
         local_risk = self.calculate_risk(position)
@@ -51,6 +73,7 @@ class Cave:
             risk += local_risk
             new_path = path.copy()
             new_path.append(position)
+            self.draw(new_path)
             return (risk, new_path)
 
         return min([total_risk(adj) for adj in self.adjacents(position)], key=operator.itemgetter(0))
@@ -61,33 +84,37 @@ class Cave:
         min_path[1].append(start)
         return min_path
 
+def main(stdscr, args):
+    if stdscr:
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    input = fileinput.FileInput(files=args.files)
+
+    cave = Cave(input)
+    cave.set_window(stdscr)
+    min_risk, min_path = cave.lowest_risk()
+    cave.draw(min_path)
+
+    if stdscr:
+        stdscr.addstr(cave.rows, 0, '{}'.format(min_risk))
+        stdscr.refresh()
+        stdscr.getkey()
+
+    return min_risk
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='*')
     parser.add_argument('--show-path', action='store_true')
     args = parser.parse_args()
 
-    input = fileinput.FileInput(files=args.files)
-
-    cave = Cave(input)
     sys.setrecursionlimit(4000)
-    min_risk, min_path = cave.lowest_risk()
 
-    attr_str = {
-        False : attr('reset'),
-        True  : attr('bold'),
-    }
-
-    for x in range(size_factor*cave.rows):
-        last_on_path = False
-        for y in range(size_factor*cave.cols):
-            position = (x, y)
-            risk = cave.calculate_risk(position)
-            on_path = position in min_path
-            if on_path ^ last_on_path and args.show_path:
-                sys.stderr.write('{}'.format(attr_str[on_path]))
-            sys.stderr.write('{}'.format(risk))
-            last_on_path = on_path
-        sys.stderr.write('{}\n'.format(attr_str[False]))
-
-    print(min_risk)
+    if args.show_path:
+        try:
+            print(curses.wrapper(main, args))
+        except KeyboardInterrupt:
+            sys.stderr.write('search interrupted\n')
+    else:
+        print(main(None, args))
